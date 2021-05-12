60d789477816ece85fd02c0b74d3faeb5042a051
Oliver Jumpertz <oliver.jumpertz@gmail.com>
2021-05-12 20:13:56 +0200

    Rename __layout.svelte to $layout.svelte to fix a bug where local development can't find the layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,1 @@
-<script>
-	import Header from '$lib/Header/index.svelte'
-	import Footer from '$lib/Footer/index.svelte';	
-	import '../app.postcss';
-</script>
-
-<main>
-	<div class="flex flex-col items-center justify-center w-screen h-screen">
-		<div class="flex flex-col items-center justify-center w-3/4 lg:w-5/12">
-			<Header />
-			<div class="flex flex-col items-center justify-center w-full py-5 space-y-3 border-t border-b border-gray-300">
-				<slot />
-			</div>
-			<Footer />
-		</div>
-	</div>
-</main>
+$layout.svelte
