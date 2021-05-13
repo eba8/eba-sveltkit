b78c4be654cddca12fc4b682bb9b795557ec961b
Edan Ben-Atar <benatare@gmail.com>
2021-05-12 23:13:06 -0400

    investments

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -8,6 +8,7 @@
             <li class="px-5 py-1 border border-gray-900 rounded-md focus:outline-none hover:bg-gray-900 hover:text-white" class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
             <a sveltekit:prefetch href="/about"><li class="px-5 py-1 border border-gray-900 rounded-md focus:outline-none hover:bg-gray-900 hover:text-white" class:active={$page.path === '/about'}>About</li></a>
             <a sveltekit:prefetch href="/projects"><li class="px-5 py-1 border border-gray-900 rounded-md focus:outline-none hover:bg-gray-900 hover:text-white" class:active={$page.path === '/projects'}>Projects</li></a>
+            <a sveltekit:prefetch href="/investments"><li class="px-5 py-1 border border-gray-900 rounded-md focus:outline-none hover:bg-gray-900 hover:text-white" class:active={$page.path === '/investments'}>Investments</li></a>
         </ul>
     </nav>
 </div>
