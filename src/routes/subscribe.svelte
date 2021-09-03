73752c9de06c3682b79c9f2515b286f6d31bb99f
Edan Ben-Atar <benatare@gmail.com>
2021-09-02 23:28:36 -0400

    update subscribe widget

--- src/routes/subscribe.svelte
+++ src/routes/subscribe.svelte
@@ -1,19 +1,47 @@
 
 <svelte:head>
-	<!-- MailerLite Universal -->
-    <script>
-    (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
-    var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-    f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
-    var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-    _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
-    
-    var ml_account = ml('accounts', '1989748', 'y8q2s0r8j3', 'load');
-    </script>
-    <!-- End MailerLite Universal -->
+
 </svelte:head>
 
-<div class="ml-form-embed"
-  data-account="1989748:y8q2s0r8j3"
-  data-form="3869293:a3n1q3">
+<div class="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-md">
+    
+    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Follow along! :)
+    </h2>
+    <p class="mt-2 text-center text-sm text-gray-600">
+      My thoughts, my ideas, and free giveaways. No spam.
+    </p>
+  </div>
+
+  <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <form class="space-y-6" action="https://www.getrevue.co/profile/edanbenatar/add_subscriber" method="post">
+        <div>
+          <div class="mt-1">
+            <input id="member_first_name" name="member[first_name]" type="text" placeholder="First name" required class="revue-form-field appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm">
+          </div>
+        </div>
+
+        <div>
+          <div class="mt-1">
+            <input id="member_last_name" placeholder="Last name" name="member[last_name]" type="text" required class="revue-form-field appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm">
+          </div>
+        </div>
+
+        <div>
+          <div class="mt-1">
+            <input id="email" name="member[email]" type="email" placeholder="you@email.com" autocomplete="email" required class="revue-form-field appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm">
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            Subscribe
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="mt-5 px-5 text-center text-gray-600">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+  </div>
 </div>
