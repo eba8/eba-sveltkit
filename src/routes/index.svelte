ed2d202b282e3548d85573095a077d41fcbee27f
Edan Ben-Atar <benatare@gmail.com>
2021-12-31 11:07:42 -0500

    add WordPress element

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -181,6 +181,32 @@
 								</svg>
 							</a>
 						</div>
+						<div>
+							<a
+								href="https://gumroad.com/a/604198003/kSrqD"
+								class="flex justify-between px-5 py-2 text-gray-800 transition-all border border-gray-300 rounded-full hover:border-gray-500 hover:text-gray-500"
+							>
+								<span class="font-bold text-md">Learn WordPress with Nat</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-7 w-7"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path d="M12 14l9-5-9-5-9 5 9 5z" />
+									<path
+										d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+									/>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+									/>
+								</svg>
+							</a>
+						</div>
 					</div>
 				{/if}
 
